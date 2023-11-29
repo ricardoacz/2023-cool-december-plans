461bf575e39afa680001094b7165b94e6e56a583
Ricardo Castillo <ricardoacz20@gmail.com>
2023-11-28 19:07:44 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import react, {useState, useEffect} from 'react'
+import bambi from './images/ice-skating-bambi.gif'
+import ohcanada from './images/ohcanada.gif'
 
 function App() {
+
+  const [image, setImage] = useState(null)
+
+  function handleChangeBambi () {
+    setImage(bambi)
+  }
+
+  function handleChangeCanada () {
+    setImage(ohcanada)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tia's Festive Itinerary</h1>
+
+      <div>
+        <img src={image}/>
+      </div>
+
+      <section className='list'>
+        <section className='item' onClick={handleChangeBambi}>
+          <section>
+            <h3>Dec 09</h3>
+          </section>
+         <section>
+            <h3>Nathan Philips Square Skate</h3>
+            <h4> 6 PM - 9 PM | BYO SKATES OR RENT</h4>
+          </section>
+        </section>
+
+        <section className='item' onClick={handleChangeCanada}>
+          <section>
+            <h3>Dec 16</h3>
+          </section>
+         <section>
+            <h3>Winterfest at Wonderland</h3>
+            <h4> 5 PM - 10 PM | 24.99 W/ 4 + PPL</h4>
+          </section>
+        </section>
+        
+      </section>
     </div>
   );
 }
